72c4b001491d4345c88e41d2ec4d07407dd0a66c
davoodmehraban89 <57283229+davoodmehraban89@users.noreply.github.com>
2026-08-27 17:25:25 +0330

    feat(invoice): add assets/js/firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,8 @@
+window.FINORA_FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyCIndzPklwMiFl_mIg_J3WYjBj3bUsbwwY',
+  authDomain: 'finora-49289.firebaseapp.com',
+  projectId: 'finora-49289',
+  storageBucket: 'finora-49289.firebasestorage.app',
+  messagingSenderId: '469630865556',
+  appId: '1:469630865556:web:f6f4241f066e3b5f466481'
+};
